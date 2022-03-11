cae3c8a9c45dbcac6b5f45a83b0787435589f762
jbuendia1y <jgamer669@gmail.com>
2022-03-11 09:15:27 -0500

    Build gh-pages folder

--- build.sh
+++ build.sh
@@ -1,29 +1,50 @@
 DIR=$(pwd)
+MAIN_PROJECT="/challenges-main-page"
+
+GH_PAGES_FOLDER="$DIR/gh-pages"
+
+if [ -d $GH_PAGES_FOLDER ]; then
+    rm -r $GH_PAGES_FOLDER
+fi
+
 
 echo Work Directory $DIR
 
 PROJECTS=("/rest-countries-api-vue" "/social-media-dashboard-theme-switcher")
 
-cd $DIR/rest-countries-api-vue
-npm run build
+NPM_PROJECTS=("/rest-countries-api-vue")
 
-cd $DIR/social-media-dashboard-theme-switcher
+for i in ${NPM_PROJECTS[@]};do
+    cd $DIR$i
+    npm i
+    npm run build  
+done
+
+YARN_PROJECTS=("/social-media-dashboard-theme-switcher")
+
+for i in ${YARN_PROJECTS[@]};do
+    cd $DIR$i
+    yarn
+    yarn build
+done
+
+cd $DIR$MAIN_PROJECT
+yarn
 yarn build
 
 cd $DIR
-{
-    mkdir github_pages
-    echo github_pages folder created !!
-} || {
-    echo github_pages folder is already exist .
-}
+mkdir $GH_PAGES_FOLDER
+
+mv -v $DIR$MAIN_PROJECT/dist/* $GH_PAGES_FOLDER/
+echo MAIN_PAGE moved !!
 
 {
     for i in ${PROJECTS[@]};do
-        mv $DIR$i/dist $i
+        mv $DIR$i/dist $GH_PAGES_FOLDER/$i
+        echo $i moved
     done
 } || {
     echo 
 }
 
-echo Finished works :)
+echo Finished works !
