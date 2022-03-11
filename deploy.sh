ea8416fd924f4783c99eb609f12380fcae6a9748
jbuendia1y <jgamer669@gmail.com>
2022-03-11 18:45:08 -0500

    Remove gh-pages Se removerá la compilación de gh-pages de la rama main

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@ bash ./build.sh
 
 git add gh-pages && git commit -m "Deploy gh-pages"
 git subtree push --prefix gh-pages origin gh-pages
+
+rm -r gh-pages
+git add . && git commit -m "Removing gh-pages build"
+git push origin main
