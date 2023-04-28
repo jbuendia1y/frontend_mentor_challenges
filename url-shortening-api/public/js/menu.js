2f28450dab1c795d4ce7804c89a16281ba34c6e1
jbuendia1y <jgamer669@gmail.com>
2023-04-28 11:48:25 -0500

    Improve performance with ViteJS

--- url-shortening-api/public/js/menu.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementsByClassName('menu__img')[0]
-const menu = document.getElementsByClassName('menu__box')[0]
-
-menuToggle.addEventListener('click',()=>{
-    menu.classList.toggle('menu__active')
-})
-
