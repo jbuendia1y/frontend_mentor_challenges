4d2d5421558b5ea08cb6ec4e8d14e20764b6d53e
jbuendia1y <jgamer669@gmail.com>
2023-12-16 17:55:25 -0500

    Upgrade deploy script

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 const ghpages = require("gh-pages");
 const path = require("path");
 const rimraf = require("rimraf");
-const exec = require("child_process").exec;
-
-const execBuild = () => {
-  return new Promise((resolve, reject) => {
-    const timeLabel = "Compilado";
-    console.time(timeLabel);
-    const buildScript = exec("bash ./build.sh");
-    buildScript.on("close", (code, signal) => {
-      console.timeEnd(timeLabel);
-      if (code > 0) {
-        reject(
-          new Error(
-            signal
-              ? signal.toString()
-              : "Ocurrió un error en el proceso de compilado"
-          )
-        );
-        return;
-      }
-      resolve();
-    });
-  });
-};
 
 async function main() {
   const ghpagesDir = path.join(__dirname, "./gh-pages");
-  await execBuild();
   const timeLabel = "Publicado a Github Pages";
 
   console.time(timeLabel);
-  ghpages.publish(ghpagesDir, (err) => {
+  await ghpages.publish(ghpagesDir, (err) => {
     if (!err) {
       rimraf.sync(ghpagesDir);
-    }
-    console.log(err);
+    } else console.log(err);
+
     console.timeEnd(timeLabel);
   });
 }
